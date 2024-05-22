141fc34a7bf5c4e2d1d0a4c9cee9566db5e54dee
maks byzovskiy <byzovskiy290808@gmail.com>
2024-05-22 18:13:55 +0300

    connect registration on test for players to server

--- apps/tests/urls.py
+++ apps/tests/urls.py
@@ -6,5 +6,7 @@ app_name = 'tests'
 urlpatterns = [
     path('test/', views.test, name='test'),
     path('create/test/', views.create_test, name='create_test'),
-    path('delete/test/<int:test_id>/', views.delete_test, name='delete_test')
+    path('delete/test/<int:test_id>/', views.delete_test, name='delete_test'),
+    
+    path('registration/<int:test_id>/', views.registration_test, name='registration_test')
 ]
