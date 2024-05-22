856b329afa83790b2b80eb4d7a54f5fe5c7ca591
maks byzovskiy <byzovskiy290808@gmail.com>
2024-05-22 18:01:48 +0300

    add modules Test, Registration

--- apps/tests/models.py
+++ apps/tests/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Test(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authors_tests', null=False)
+    topic = models.CharField(max_length=20)
+    description = models.TextField(max_length=255, blank=True, null=True)
+    
+    created_at = models.DateTimeField(default=timezone.now, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.topic
+    
+    class Meta:
+        ordering=['-created_at']
+    
+    
+class Registration(models.Model):
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    
+    def __str__(self):
+        return f'Реєстрація на тест {self.test.topic} {self.user.username}'
+    
+    class Meta:
+        verbose_name='Реєстрація',
+        verbose_name_plural='Реєстрації'
